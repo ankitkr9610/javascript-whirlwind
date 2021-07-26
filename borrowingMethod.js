7582b8b43e583d20dea424442db7461bed3e3aa7
ankitkr9610 <86177505+ankitkr9610@users.noreply.github.com>
2021-07-26 08:51:15 +0530

    updating borrowing method
    adding call on Object.prototype.hasOwnProperty

--- borrowingMethod.js
+++ borrowingMethod.js
@@ -10,5 +10,5 @@ let map = {one: true, two: true, hasOwnProperty: true};
 // Fix this call
 console.log(map.hasOwnProperty("one"));
 //Fixed call
-console.log(Object.prototype.hasOwnProperty(map, "one"));  
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));  
 // → true
